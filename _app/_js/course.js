9860cdb0d29f481680cc8dfcd23069994efbbb12
pavelp21 <pavelvp2182@yandex.ru>
2020-05-30 17:24:00 +0500

    Course page is done

---
+++ _app/_js/course.js
@@ -0,0 +1,55 @@
+//@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend jquery.fancybox.min.js
+
+$(document).ready(function () {
+    /* Якорь */
+    $(function () {
+        $("a[href='#prices']").click(function (h) {
+            h.preventDefault();
+            var f = $(this).attr("href"),
+                g = $(f).offset().top;
+            $("body,html").animate({
+                scrollTop: g
+            }, 1500)
+        });
+    });
+    $('.modal-up').fancybox();
+    $(function () {
+        $("[data-fancybox]").fancybox({
+            buttons: [
+                'slideShow',
+                'share',
+                'zoom',
+                'fullScreen',
+                // 'download',
+                'close'
+            ],
+            speed: 330,
+            loop: true,
+            opacity: "auto",
+            // autoScale: true,
+            mouseWheel: true,
+            transitionEffect: 'slide'
+        });
+    });
+    function videos() {
+        $(".video-wrapper-revs img").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1"  allowfullscreen></iframe>')
+        });
+    }
+    videos();
+    $(function () {
+        $('#video-revs').on('slide.bs.carousel', function () {
+            $('.video-wrapper-revs iframe').each(function () {
+                var l = $(this).parent().attr('data-img');
+                $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+            });
+            videos();
+        });
+    });
+
+
+    /*Конец документа*/
+});
